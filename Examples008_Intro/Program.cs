a4357ec60dc48256c696f7f6cd6a9c2d3c866b74
Olga Bel <olza91@mail.ru>
2022-10-02 16:27:06 +0300

    функции

---
+++ Examples008_Intro/Program.cs
@@ -0,0 +1,22 @@
+﻿// Это классический стихийный подход, в папке 018 будет функция
+int a1 = 10;
+int a2 = 15;
+int a3 = 27;
+int a4 = 42;
+int a5 = 1;
+int a6 = 17;
+int a7 = 33;
+int a8 = 110;
+int a9 = 5;
+
+int max = a1;
+if (a2>max) max = a2;
+if (a3>max) max = a3;
+if (a4>max) max = a4;
+if (a5>max) max = a5;
+if (a6>max) max = a6;
+if (a7>max) max = a7;
+if (a8>max) max = a8;
+if (a9>max) max = a9;
+
+Console.WriteLine(max);
---
+++ Examples018_Intro/Program.cs
@@ -0,0 +1,25 @@
+﻿// показана Функция (МЕТОД) нахождения максимального числа. сравниваем по 3 числа и битва победителей
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+int a1 = 10;
+int a2 = 15;
+int a3 = 27;
+int a4 = 420;
+int a5 = 1;
+int a6 = 17;
+int a7 = 33;
+int a8 = 110;
+int a9 = 5;
+
+int max1 = Max(a1,a2,a3);
+int max2 = Max(a4,a5,a6);
+int max3 = Max(a7,a8,a9);
+int MAX = Max(max1,max2,max3);
+
+Console.WriteLine(MAX);
